196a8d581589ca9e71efc12f1747c4f48140208f
Robby2012 <robby.upm@gmail.com>
2013-03-05 20:51:21 +0100

    weeMail - First design almost finished

--- static/script.js
+++ static/script.js
@@ -1,6 +1,41 @@
 
 wz.app.addScript( 8, 'common', function( win ){
     
-    
+    $(win)
+	
+		.on( 'click', '.account', function(){
+			if( $(this).hasClass('display') ){
+				$(this).removeClass('display');
+				$(this).transition({ height: 38 }, 250);
+			}else{
+				$('.display').transition({ height: 38 }, 250).removeClass('display');
+				$(this).addClass('display');
+				$(this).transition({ height: 250 }, 250);
+			}
+		})
+		
+		.on( 'click', '.account article', function(e){
+			e.stopPropagation();
+		})
+		
+		.on( 'click', '.message', function(){
+			if( !$(this).hasClass('selected') ){
+				$('.selected').removeClass('selected');
+				$(this).addClass('selected');
+			}		
+		})
+		
+		.on( 'click', 'input', function(e){
+			e.stopPropagation();
+		})
+		
+		.on( 'click', '.message-star', function(e){
+			e.stopPropagation();
+			if( $(this).hasClass('active') ){
+				$(this).removeClass('active');
+			}else{
+				$(this).addClass('active');
+			}
+		});
 
 });
